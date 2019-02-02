753f91e618d4be80c57585846daed9933e0bfdc4
alirezatab <alirezatabrizi10@gmail.com>
2019-02-01 21:10:36 -0800

    #1_TwoSum Optimized from Brute Force

--- #1_TwoSum.playground/Contents.swift
+++ #1_TwoSum.playground/Contents.swift
@@ -1,5 +1,34 @@
 import UIKit
+/*
+ check if two is in disctioanry,
+ 9-2 = 7
+ add 8 to dectionary or set
+ 
+ 
+ 
+ key  value
+ 7     0
+ 
+ return 0, currentIndex
+ 
+ */
 
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var dic = [Int:Int]()
+        for i in 0..<nums.count {
+            let num = nums[i]
+            if dic[num] != nil {
+                return [dic[num]!,i]
+            }
+            
+            dic[target-num] = i
+        }
+        return []
+    }
+}
+
+/* Brute Force
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
         var finalArr: [Int] = []
@@ -15,6 +44,7 @@ class Solution {
         return []
     }
 }
+ */
 
 let solution = Solution()
 print(solution.twoSum([2,7,11,15], 9))
