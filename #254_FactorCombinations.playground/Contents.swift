7ab72f444a2ab891b4d8ac24807323c99a52c367
alirezatab <alirezatabrizi10@gmail.com>
2019-04-04 16:13:58 -0700

    #254_FactorCombinations completed

---
+++ #254_FactorCombinations.playground/Contents.swift
@@ -0,0 +1,29 @@
+/*
+ Runtime: 8 ms, faster than 100.00% of Swift online submissions for Factor Combinations.
+ Memory Usage: 19.3 MB, less than 100.00% of Swift online submissions for Factor Combinations.
+ */
+import Darwin
+
+class Solution {
+    func getFactors(_ n: Int) -> [[Int]] {
+        var res = [[Int]]()
+        guard n>3 else {return res}
+        
+        func createFactors(_ c: [Int], _ start: Int,_ end: Int) {
+            
+            let sqrtN = Int(ceil(sqrt(Double(end))))
+            
+            if start <= sqrtN {
+                for i in start...sqrtN {
+                    if end%i == 0 && end/i >= i {
+                        res.append(c+[i]+[end/i])
+                        createFactors(c+[i], i, end/i)
+                    }
+                }
+            }
+        }
+        
+        createFactors([], 2, n)
+        return res
+    }
+}
