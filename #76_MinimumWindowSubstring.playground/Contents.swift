e03e6f002cda475d8e9a3d88e24cf249773a0c03
alirezatab <alirezatabrizi10@gmail.com>
2019-04-07 00:08:13 -0700

    #76_MinimumWindowSubstring completed

--- #76_MinimumWindowSubstring.playground/Contents.swift
+++ #76_MinimumWindowSubstring.playground/Contents.swift
@@ -1,67 +1,60 @@
 /*
  Time: o(N)
  space: o(n)
+ 
+ Runtime: 80 ms, faster than 83.93% of Swift online submissions for Minimum Window Substring.
+ Memory Usage: 20.1 MB, less than 60.00% of Swift online submissions for Minimum Window Substring.
  */
 class Solution {
     func minWindow(_ s: String, _ t: String) -> String {
         guard s.count>=t.count else {return ""}
         
-        var dic = [Character:Int]()
+        var dic = [String.Element:Int]()
+        let sChars = Array(s)
+        let tChars = Array(t)
         
-        for char in s {
-            dic[char] = 0
-        }
-        for char in t {
+        for char in tChars {
             if let val = dic[char] {
                 dic[char] = val+1
             } else {
-                return ""
+                dic[char] = 1
             }
         }
         
-        var start = 0, end = 0
+        var start = 0
         var minStart = 0, minLength = Int.max
         var numOfTargets = t.count
         
-        while end < s.count {
-            print(end)
-            let endIndex = s.index(s.startIndex, offsetBy: end)
-            let endChar = s[endIndex]
-
-            if let curOcurrance = dic[endChar] {
+        for end in 0..<sChars.count {
+            
+            if let curOcurrance = dic[sChars[end]] {
                 if curOcurrance > 0 {
                     numOfTargets -= 1
                 }
-                dic[endChar] = curOcurrance-1
+                dic[sChars[end]] = curOcurrance-1
             }
-            print("here")
-
+            
             while numOfTargets == 0 {
                 if minLength > end-start+1 {
                     minLength = end-start+1
                     minStart = start
                 }
-                let startIndex = s.index(s.startIndex, offsetBy: start)
-                let startChar = s[startIndex]
                 
-                if let curOccurance = dic[startChar] {
-                    if curOccurance == 0 { // or >=
+                if let curOccurance = dic[sChars[start]] {
+                    if curOccurance == 0 {
                         numOfTargets += 1
                     }
-                    dic[startChar] = curOccurance+1
+                    dic[sChars[start]] = curOccurance+1
                 }
                 start+=1
             }
-            end += 1
         }
-        if minLength != Int.max {
-            let startWindowIndex = s.index(s.startIndex, offsetBy: minStart)
-            let endWindowIndex = s.index(s.startIndex, offsetBy: minStart+minLength)
-            return String(s[startWindowIndex..<endWindowIndex])
-        }
-        return ""
+        
+        return minLength == Int.max ? "" : String(sChars[minStart..<minStart+minLength])
     }
 }
+
+
 
 let sol = Solution()
 sol.minWindow("babb", "baba")
