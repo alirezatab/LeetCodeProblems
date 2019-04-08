21b4e120ec14a4ba6ce128d4bb6af9a2f9773a97
alirezatab <alirezatabrizi10@gmail.com>
2019-04-07 20:15:08 -0700

    #605_CanPlaceFlowers updated and removed some useless variables

--- #605_CanPlaceFlowers.playground/Contents.swift
+++ #605_CanPlaceFlowers.playground/Contents.swift
@@ -1,9 +1,14 @@
+//////////////////////////////////////////////////
+///////////////////// TRY 1 //////////////////////
+//////////////////////////////////////////////////
 /*
  Runtime: 132 ms, faster than 100.00% of Swift online submissions for Can Place Flowers.
  Memory Usage: 18.9 MB, less than 100.00% of Swift online submissions for Can Place Flowers.
+ Time: o(n)
+ space: o(n)
  */
 
-class Solution {
+class Solution1 {
     func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
         var counter = 1
         var nFlowers = 0
@@ -24,6 +29,40 @@ class Solution {
                 }
                 end += 1
             }
+        }
+        if counter == 2 {
+            nFlowers += 1
+        }
+        
+        return n <= nFlowers
+    }
+}
+//////////////////////////////////////////////////
+///////////////////// TRY 2 //////////////////////
+//////////////////////////////////////////////////
+/*
+Runtime: 112 ms, faster than 100.00% of Swift online submissions for Can Place Flowers.
+Memory Usage: 19.2 MB, less than 100.00% of Swift online submissions for Can Place Flowers.
+Time: o(n)
+space: o(n)
+*/
+class Solution2 {
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        var counter = 1
+        var nFlowers = 0
+        var end = 0
+        
+        while end<flowerbed.count {
+            if flowerbed[end] != 0 { // 1 found
+                counter = 0
+            } else { // zero found
+                counter += 1
+                if counter == 3 {
+                    nFlowers += 1
+                    counter = 1
+                }
+            }
+            end += 1
         }
         if counter == 2 {
             nFlowers += 1
