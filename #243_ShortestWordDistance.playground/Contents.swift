36f00d109f9df3bebfb700f06588ee787cebd876
alirezatab <alirezatabrizi10@gmail.com>
2019-04-10 21:53:55 -0700

    update and or cleaned up code by removing useless variables

--- #243_ShortestWordDistance.playground/Contents.swift
+++ #243_ShortestWordDistance.playground/Contents.swift
@@ -8,21 +8,17 @@ Memory Usage: 19.8 MB, less than 25.00% of Swift online submissions for Shortest
 class Solution {
     func shortestDistance(_ words: [String], _ word1: String, _ word2: String) -> Int {
         var shortestDis = Int.max
-        var w1found = false
-        var w2found = false
         var w1index = -1
         var w2index = -1
         
         for i in 0..<words.count {
             if words[i] == word1 {
-                w1found = true
                 w1index = i
             } else if words[i] == word2 {
-                w2found = true
                 w2index = i
             }
             
-            if w1found && w2found {
+            if w1index != -1 && w2index != -1 {
                 let dis = abs(w1index - w2index)
                 if dis < shortestDis {
                     shortestDis = dis
