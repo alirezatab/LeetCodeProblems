3e228c2d9f004a5522d379736fa7033dddfc17e7
alirezatab <alirezatabrizi10@gmail.com>
2019-06-03 18:11:32 -0700

    completed #986_IntervalListIntersections

---
+++ #986_IntervalListIntersections.playground/Contents.swift
@@ -0,0 +1,34 @@
+/*
+ Runtime: 224 ms, faster than 38.18% of Swift online submissions for Interval List Intersections.
+ Memory Usage: 21.2 MB, less than 7.32% of Swift online submissions for Interval List Intersections.
+ 
+ 
+ Time = o(n)
+ Space = o(n)
+ */
+class Solution {
+    func intervalIntersection(_ A: [[Int]], _ B: [[Int]]) -> [[Int]] {
+        var i = 0
+        var j = 0
+        var res = [[Int]]()
+        
+        while i<A.count && j<B.count {
+            let start = max(A[i][0], B[j][0])
+            let end = min(A[i][1], B[j][1])
+            
+            if start<=end {
+                res.append([start,end])
+            } else if start==end {
+                res.append([start,end])
+            }
+            
+            if A[i][1]>B[j][1]{
+                j+=1
+            } else {
+                i+=1
+            }
+        }
+        
+        return res
+    }
+}
