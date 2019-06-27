3b6abb343011fc67f14fec3a36e4ae36beebb3ce
alirezatab <alirezatabrizi10@gmail.com>
2019-06-26 19:06:40 -0700

    updated time and space, cleaned old code and implemented new code with new leetcode version

--- #253_MeetingRoomsII.playground/Contents.swift
+++ #253_MeetingRoomsII.playground/Contents.swift
@@ -40,6 +40,9 @@
 /*
  Runtime: 80 ms, faster than 98.45% of Swift online submissions for Meeting Rooms II.
  Memory Usage: 19.5 MB, less than 7.69% of Swift online submissions for Meeting Rooms II.
+ 
+ Time: o(nlogn)
+ Space: o(n)
  */
 
 /**
@@ -59,8 +62,8 @@ class Solution {
         guard intervals.count > 0 else {return 0}
         guard intervals.count > 1 else {return 1}
         
-        var startTimes = intervals.map {$0.start}.sorted()
-        var endTimes = intervals.map {$0.end}.sorted()
+        let startTimes = intervals.map {$0.start}.sorted()
+        let endTimes = intervals.map {$0.end}.sorted()
         
         var startTimeIndex = 0
         var endTimeIndex = 0
@@ -69,13 +72,68 @@ class Solution {
         while startTimeIndex<startTimes.count && endTimeIndex<endTimes.count {
             if startTimes[startTimeIndex] < endTimes[endTimeIndex] {
                 numberOfRooms+=1
-                startTimeIndex+=1
             } else {
-                startTimeIndex+=1
                 endTimeIndex+=1
             }
+            startTimeIndex+=1
+
         }
         
+        return numberOfRooms
+    }
+}
+
+/*
+ [[0, 30],[5, 10],[15, 20]]
+ 
+ startTime = [0,5,15]
+ endTime = [30,10,20]
+ 
+ sort both
+ counter = 1
+ i
+ startTime = [0,5,15]
+ j
+ endTime = [10,20,30]
+ 
+ if startTime[i] is < endTime[j], inc i
+ 
+ i
+ startTime = [0,5,15]
+ j
+ endTime = [10,20,30]
+ 
+ else {counter+=1, inc j }
+ 
+ startTime = [0,5,15]
+ 
+ endTime = [10,20,30]
+ 
+ 
+ 
+ */
+
+class Solution1 {
+    func minMeetingRooms(_ intervals: [[Int]]) -> Int {
+        guard intervals.count > 0 else {return 0}
+        guard intervals.count > 1 else {return 1}
+        
+        var startTimes = intervals.map {$0.first!}.sorted()
+        var endTimes = intervals.map {$0.last!}.sorted()
+        
+        var startTimeIndex = 0
+        var endTimeIndex = 0
+        var numberOfRooms = 0
+        
+        while startTimeIndex<startTimes.count && endTimeIndex<endTimes.count {
+            if startTimes[startTimeIndex] < endTimes[endTimeIndex] {
+                numberOfRooms += 1
+            } else {
+                endTimeIndex += 1
+            }
+            startTimeIndex+=1
+            
+        }
         return numberOfRooms
     }
 }
