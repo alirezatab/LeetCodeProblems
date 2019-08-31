3ae4a51f0e29a03886f774b7bf1f629e7650edb3
alirezatab <alirezatabrizi10@gmail.com>
2019-08-30 17:52:57 -0700

    #322_CoinChange.playground Dynamic programing bottom Up_Tabulation completed

--- #322_CoinChange.playground/Contents.swift
+++ #322_CoinChange.playground/Contents.swift
@@ -78,3 +78,48 @@ class Solution {
         return memo[amount] == Int.max ? -1 : memo[amount]!
     }
 }
+
+/*
+    coins [1,2,5]
+    Tabluation
+    [0,1,2,3,4,5,6,7,8,9,10,11] array size
+    [0,1,1,2,2,1,2,2,3,3,2,2] (index-coin) need to be above 0 - grab value there and add one to it
+ 
+ option 1:
+ Runtime: 116 ms, faster than 94.89% of Swift online submissions for Coin Change.
+ Memory Usage: 21 MB, less than 33.33% of Swift online submissions for Coin Change.
+ 
+ option 2:
+ Runtime: 76 ms, faster than 100.00% of Swift online submissions for Coin Change.
+ Memory Usage: 20.8 MB, less than 33.33% of Swift online submissions for Coin Change.
+ 
+ Time: O(S∗n). On each step the algorithm finds the next F(i) in n iterations, where 1≤i≤S. Therefore in total the iterations are S∗n.
+ Space: O(n). We use extra space for the memoization array.
+*/
+
+class Solution1 {
+    func coinChange(_ coins: [Int], _ amount: Int) -> Int {
+        //repeat can be Int.max
+        guard amount > 0 else {return 0}
+        var resultArray = [Int](repeating: amount+1, count: amount+1)
+        
+        resultArray[0] = 0
+        
+        for coin in coins {
+            for i in 1...amount {
+                if i >= coin {
+                    // Option 1: slower a bit
+                    // let minVal = min(resultArray[i-coin]+1, resultArray[i])
+                    // resultArray[i] = minVal
+                    
+                    // Option 2: faster a bit
+                    if resultArray[i-coin] < resultArray[i] {
+                        resultArray[i] = resultArray[i-coin] + 1
+                    }
+                }
+            }
+        }
+        
+        return resultArray[amount] == amount+1 ? -1 : resultArray[amount]
+    }
+}
