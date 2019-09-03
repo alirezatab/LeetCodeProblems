2576a2b0f08becd2d53daaf8139959ccc7862f8c
alirezatab <alirezatabrizi10@gmail.com>
2019-09-02 21:32:56 -0700

    added diagrams and time and space

--- #5_LongestPalindromicSubstring.playground/Contents.swift
+++ #5_LongestPalindromicSubstring.playground/Contents.swift
@@ -1,3 +1,97 @@
+/*
+  "babad" i=0  arr.count = 5, palindromeStr = ""
+   |      Li, Ri = i
+   Li     check if the value at i is the same, if it is same, it is palindrome,
+   Ri         in Ri, dec Li
+
+  "babad"
+|   |
+Li  Ri     return "b"
+
+  pal = b
+
+  "babad" Li = i, Ri = i+1
+   ||
+  LiRi      break and return "a"
+
+  inc i
+  
+  "babad"
+    |
+   Li
+   Ri
+  
+  "babad" isPalindrome is true
+   | |
+  Li Ri
+
+  "babad" out of index, so return "bab"
+ |    |
+ Li   Ri
+
+  if return string.count > palin.count -> replace palindoem string
+   
+
+  "babad" isPalindrome is true
+    ||
+   LiRi
+
+  ...
+
+  "cbbd"
+   |
+   Li
+   Ri   pal = "c"
+
+  "cbbd"
+   ||
+  Li
+    Ri  false return "c"
+
+  inc i
+  "cbbd"
+    |
+   Li
+   Ri true
+  
+  "cbbd"  return "b" -> no change cause of count logic
+   | |
+   Li
+     Ri
+
+  "cbbd"
+    ||
+   Li
+     Ri true, dec inc Li, Ri
+
+  "cbbd"
+   |  |
+   Li
+     Ri false, update palindomeStr = "bb"
+
+  
+
+  inc i
+  "cbbd"
+     |
+     Li
+     Ri true, inc dec Li Ri
+
+  "cbbd"
+    | |
+    Li
+      Ri false, return "b"
+  */
+
+/*
+ Runtime: 1000 ms, faster than 28.63% of Swift online submissions for Longest Palindromic Substring.
+ Memory Usage: 21 MB, less than 100.00% of Swift online submissions for Longest Palindromic Substring.
+ 
+ Time: o(n^3)
+ Space: o(1)
+ 
+ */
+
 class Solution {
     func longestPalindrome(_ s: String) -> String {
         var res = ""
